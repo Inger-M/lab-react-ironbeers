c40279f25518acbb550790eadb89847a9f0d1250
Inger-Merete Bruhn Cliquet <Inger-Merete.Bruhn@LEGO.com>
2022-06-04 17:05:50 +0200

    started

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import HomePage from "./pages/HomePage";
+import ListBeersPage from "./pages/ListBeersPage";
+import NewBeerPage from "./pages/NewBeerPage";
+import RandomBeerPage from "./pages/RandomBeerPage";
+import SingleBeerPage from "./pages/SingleBeerPage";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
